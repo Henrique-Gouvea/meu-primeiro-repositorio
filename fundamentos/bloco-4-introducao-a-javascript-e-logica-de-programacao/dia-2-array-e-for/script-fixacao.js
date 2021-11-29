1fd7acd2fa71ae65f55878d941f5f6d311dbd74a
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2021-11-29 15:52:44 -0300

    Exercicios fixacao FOR e FOR OF

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-for/script-fixacao.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-for/script-fixacao.js
@@ -55,3 +55,20 @@ menu.push("Contato");
 console.log(menu);
 
 console.log("--------------------------Exercicio Fixacao FOR- 1 --------------------------------")
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let i=0; i<groceryList.length; i++){
+    console.log(groceryList[i]);
+}
+
+
+
+console.log("--------------------------Exercicio Fixacao FOR/OF- 1 --------------------------------")
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let names2 of names){
+    console.log(names2);
+}
