00adf7e587a436764935d8cdaadf98daf8cc625a
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2021-11-30 16:05:37 -0300

    exercicios 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/scripts.js
@@ -7,10 +7,15 @@ for (let i=0; i<n; i++){
 for(let i=0; i<n; i++){
     console.log(line);
 }
+console.log("--------------------------------Exercicio-2-------------------------------");
+
+let =3;
+line="";
 
 for (let i=0; i<n; i++){
     line=line+"*";
-    for(let i=0; i<n; i++){
-        console.log(line);
-    }
+    console.log(line);
 }
+
+console.log("--------------------------------Exercicio-2-------------------------------");
+
