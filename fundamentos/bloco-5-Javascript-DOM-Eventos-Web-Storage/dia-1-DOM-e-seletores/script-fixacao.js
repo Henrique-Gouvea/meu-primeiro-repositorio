f4932b2774a41ba45e415e0a3b078a59eb9f4b4d
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2021-12-03 15:56:31 -0300

    exercicio 2 fixacao

--- fundamentos/bloco-5-Javascript-DOM-Eventos-Web-Storage/dia-1-DOM-e-seletores/script-fixacao.js
+++ fundamentos/bloco-5-Javascript-DOM-Eventos-Web-Storage/dia-1-DOM-e-seletores/script-fixacao.js
@@ -1,6 +1,15 @@
-const paragraph = document.getElementById("paragraph");
+const paragraph = document.getElementById("h3");
 paragraph.style.color = "red";
 
-document.getElementById("page-title").innerText= "Harry Potter and Prizoner of Azkaban";
-document.getElementById("paragraph").innerText="Caution in Forbbiden forest ";
-document.getElementById("subtitle").innerText="Live a werewolf there";
+document.getElementById("page-title").innerText = "Harry Potter and Prizoner of Azkaban";
+document.getElementById("h3").innerText = "Caution in Forbbiden forest ";
+document.getElementById("subtitle").innerText = "Live a werewolf there";
+
+document.getElementsByClassName("paragraph")[0].innerText="Hypogrif in Wood";
+
+const paragraph1 = document.getElementsByClassName("paragraph")[0];
+
+paragraph.style.backgroundColor = "grey";
+
+subtitle = document.getElementsByTagName("h4")[0];
+subtitle.style.backgroundColor = "pink";
