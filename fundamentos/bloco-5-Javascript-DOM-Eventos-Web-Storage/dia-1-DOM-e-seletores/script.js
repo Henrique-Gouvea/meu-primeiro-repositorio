9f3c4676c08fedb4a863d687f2ad4e2c7dcc0917
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2021-12-03 18:31:49 -0300

    exercicio mudar cor quadrado vermelho

---
+++ fundamentos/bloco-5-Javascript-DOM-Eventos-Web-Storage/dia-1-DOM-e-seletores/script.js
@@ -0,0 +1,29 @@
+
+        // Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+        // - document.getElementById()
+        // - document.getElementsByClassName()
+        // - document.getElementsByTagName()
+
+//  Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
+
+let tagP = document.getElementsByTagName("p")[0];
+tagP.innerText = "Me vejo em uma boa empresa";
+
+//  Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+
+let mainContent = document.getElementsByClassName("main-content")[0];
+mainContent.style.backgroundColor = "rgb(76,164,109)";
+
+//  Crie uma função que mude a cor do quadrado vermelho para branco.
+
+document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
+
+//  Crie uma função que corrija o texto da tag <h1>.
+
+//  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+
+//  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+
+        
+
