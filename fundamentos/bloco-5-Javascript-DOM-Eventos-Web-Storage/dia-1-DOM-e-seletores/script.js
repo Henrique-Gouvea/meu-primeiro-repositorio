03dca910c53a2f42d5244ea266b23408e4c18c47
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2021-12-03 19:12:02 -0300

    exercicio fim

--- fundamentos/bloco-5-Javascript-DOM-Eventos-Web-Storage/dia-1-DOM-e-seletores/script.js
+++ fundamentos/bloco-5-Javascript-DOM-Eventos-Web-Storage/dia-1-DOM-e-seletores/script.js
@@ -6,26 +6,44 @@
 
 //  Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
 
-
-let tagP = document.getElementsByTagName("p")[0];
-tagP.innerText = "Me vejo em uma boa empresa";
+function mudancaEscritaP(){
+    let tagP = document.getElementsByTagName("p")[0];
+    tagP.innerText = "Me vejo em uma boa empresa";
+}
+mudancaEscritaP();
 
 //  Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 
-let mainContent = document.getElementsByClassName("main-content")[0];
-mainContent.style.backgroundColor = "rgb(76,164,109)";
+function mudancaCorMainContent(){
+    let mainContent = document.getElementsByClassName("main-content")[0];
+    mainContent.style.backgroundColor = "rgb(76,164,109)";
+}
 
 //  Crie uma função que mude a cor do quadrado vermelho para branco.
 
-document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
+function mudancaCorCenter(){
+    document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
+}
 
 //  Crie uma função que corrija o texto da tag <h1>.
-
-document.getElementsByTagName("h1")[0].innerText = "Exercício 5.1 - JavaScript";
-
+function mudancaEscritaH1(){
+    document.getElementsByTagName("h1")[0].innerText = "Exercício 5.1 - JavaScript";
+}
 //  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+
+function maisculoP(){
+    let maiuscula = document.getElementsByTagName("p")[0];
+    console.log(maiuscula.toUpperCase());
+}
+
 
 //  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 
-        
+function showPConsole(){
+    console.log(document.querySelectorAll("p"));
+    for (i = 0; i < pconsole.length; i += 1){
+        console.log(pconsole[i]);
+    }
+}   
+
 
