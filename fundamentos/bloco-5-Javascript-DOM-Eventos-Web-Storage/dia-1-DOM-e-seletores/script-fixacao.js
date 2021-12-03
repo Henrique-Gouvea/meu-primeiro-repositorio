1103ec416b3084878254eefc8f635ee52146d82d
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2021-12-03 15:27:18 -0300

    fixacao 1

---
+++ fundamentos/bloco-5-Javascript-DOM-Eventos-Web-Storage/dia-1-DOM-e-seletores/script-fixacao.js
@@ -0,0 +1,6 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText= "Harry Potter and Prizoner of Azkaban";
+document.getElementById("paragraph").innerText="Caution in Forbbiden forest ";
+document.getElementById("subtitle").innerText="Live a werewolf there";
