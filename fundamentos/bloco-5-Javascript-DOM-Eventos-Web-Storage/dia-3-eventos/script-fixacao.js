b649fad82a2a723fb1a13439fdd9cc91084677a6
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2021-12-07 18:36:10 -0300

    Exercicio 5.3 - exercicio 3

--- fundamentos/bloco-5-Javascript-DOM-Eventos-Web-Storage/dia-3-eventos/script-fixacao.js
+++ fundamentos/bloco-5-Javascript-DOM-Eventos-Web-Storage/dia-3-eventos/script-fixacao.js
@@ -14,7 +14,6 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 
 function addClassTechClick(valor){
-    let techElement = document.querySelector(".tech");
     valor.target.classList.add("tech");
 }
 
@@ -22,21 +21,29 @@ firstLi.addEventListener("click", addClassTechClick);
 secondLi.addEventListener("click", addClassTechClick)
 thirdLi.addEventListener("click", addClassTechClick);
 
-
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
-// function alterText(evento){
-//     let techElement = 
-// }
+function alterText(evento){
+    let techElement = document.querySelector('.tech');
+    techElement.innerText = evento.target.value;
+}
 
-// input.addEventListener('keyup', alterText);
+input.addEventListener('keyup', alterText);
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+
+function doubleKickH1(){
+    window.location.replace("https://www.google.com.br");
+    console.log("foi");
+}
+
+let h1 = document.querySelector("h1");
+h1.addEventListener('dblclick', doubleKickH1);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
