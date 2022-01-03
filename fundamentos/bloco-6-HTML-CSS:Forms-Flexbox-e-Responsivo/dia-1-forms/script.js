884b22db95e4a99ce70d31b99b36cd18c5afe97e
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-01-03 18:17:04 -0300

    Estilizando css

--- fundamentos/bloco-6-HTML-CSS:Forms-Flexbox-e-Responsivo/dia-1-forms/script.js
+++ fundamentos/bloco-6-HTML-CSS:Forms-Flexbox-e-Responsivo/dia-1-forms/script.js
@@ -1,5 +1,10 @@
 arrayEstados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','MT','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
 inputEstados = document.getElementById("input-estado");
+btnEnviarEmprego = document.getElementById("btn-enviar-emprego");
+btnEnviarDados = document.getElementById("btn-enviar-dados");
+divDados = document.getElementById('dados');
+divEmprego = document.getElementById('emprego');
+formEmprego = document.getElementById('form-emprego')
 
 function addEstadosInput(){
     for (let i = 0; i < arrayEstados.length; i += 1) {
@@ -12,3 +17,12 @@ function addEstadosInput(){
 }
 
 addEstadosInput();
+
+function enviar(){
+    
+    divDados.innerHtml = formEmprego.value;
+    console.log(event.value);
+    //event.preventDefault();
+}
+
+btnEnviarEmprego.addEventListener('click', enviar);
