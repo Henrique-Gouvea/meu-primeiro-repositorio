f1e25609a9e85eb3411df255d085cb43d01d2c19
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-01-12 19:02:09 -0300

    Exercicio 1 - parte 1 usando arrow functions e ternary operator

--- fundamentos/bloco-6-HTML-CSS:Forms-Flexbox-e-Responsivo/dia-6-ES6-let-const-arrow-template/script.js
+++ fundamentos/bloco-6-HTML-CSS:Forms-Flexbox-e-Responsivo/dia-6-ES6-let-const-arrow-template/script.js
@@ -1,16 +1,25 @@
-function testingScope(escopo) {
-    if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
-    } else {
-      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-  }
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
 
-  testingScope(true);
+//   testingScope(true);
+//OTIMIZANDO ABAIXO
+
+
+const testingScope = escopo => escopo ? ' ótimo, fui utilizada no escopo !' : 'Não devo ser utilizada fora meu escopo (else)';
+
+  console.log(testingScope(false));
+
+
+
 
 
 
