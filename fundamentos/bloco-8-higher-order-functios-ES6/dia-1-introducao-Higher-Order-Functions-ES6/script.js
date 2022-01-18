1c92c395e5334c486d1b3bb316d487f5a207503f
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-01-18 20:02:53 -0300

    Exercicio 3

--- fundamentos/bloco-8-higher-order-functios-ES6/dia-1-introducao-Higher-Order-Functions-ES6/script.js
+++ fundamentos/bloco-8-higher-order-functios-ES6/dia-1-introducao-Higher-Order-Functions-ES6/script.js
@@ -32,3 +32,31 @@ const result = (apostado) => {
 const apostado = (numeroSort) => numeroSort === 4;
 
 console.log(result(apostado));
+
+//----------------------------------Exer 3 --------------------------------------------------
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compare = (resp, stu, check) => {
+  return `Sua pontuação foi ${check(resp, stu)}`;
+}
+
+const check = (respOk, respStu) => {
+    let soma = 0;
+    for (let i = 0; i < respOk.length; i += 1){
+        if (respStu[i] !== 'N.A'){
+            if (respOk[i] === respStu[i]){
+                soma += 1;
+            } else {
+                soma -= 0.5;
+            }
+        }
+    }
+    return soma;
+}
+
+console.log(compare(RIGHT_ANSWERS, STUDENT_ANSWERS, check));
