f3095ae570349c76a61a6f8a5b4391c406491c9a
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-02-17 13:30:58 -0300

    exercicios react

--- desenvolvimento-front-end/bloco-10-introducao-react/dia1-react-hello-world/exercicio-day1/src/App.js
+++ desenvolvimento-front-end/bloco-10-introducao-react/dia1-react-hello-world/exercicio-day1/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const array = ['mamao', 'banana', 'laranja', 'melancia']
+
+const Task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{value}</li>
   );
+}
+
+function App() {
+  return (<ul>{array.map((item) => Task(item))}</ul>)
 }
 
 export default App;
