0e324882ae34e38e85903b1caa736a46ae4433c6
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-02-17 15:46:57 -0300

    adicionando compoenente img

--- desenvolvimento-front-end/bloco-10-introducao-react/dia2-components-react/fixacao-10-2-/src/App.js
+++ desenvolvimento-front-end/bloco-10-introducao-react/dia2-components-react/fixacao-10-2-/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hello from './components/Hello';
+import Image from './components/Image';
 
 function App() {
   return (
     <header>
       <Hello firstName="Henrique" lastName="Gouvea"/>
+      <Image source="https://s2.glbimg.com/kY7AbKs0CqXQuNd3Q65Ld-oIb58=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/A/S/EFBSAgSqyRRFei3zG3Cg/1.jpg" text="NFT monkey"/>
     </header>
   );
 }
