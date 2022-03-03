e185ea11ad3870651ba24813eab7db180457d945
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-03-03 19:17:09 -0300

    salvanado piadas

---
+++ desenvolvimento-front-end/bloco-12-ciclo-de-vida-componente-e-react-router/fixacao/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './components/Counter';
+import DadJoke from './components/jokes';
+
+function App() {
+  return (<>
+    <DadJoke />
+    <Counter/>
+  </>);
+}
+
+export default App;
